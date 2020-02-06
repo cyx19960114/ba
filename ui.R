9360dad36e72170634983d5723f18d213ec28bce
cyx19960114 <cyx19960114@foxmail.com>
2020-02-06 15:28:56 +0800

    6/2/2020 15:28 China
    updata UI

--- ui.R
+++ ui.R
@@ -18,29 +18,26 @@ u_region<-c(paste("L_Region",1:74), paste("R_Region",1:74))
 
 fluidPage(
   titlePanel("ggseg3d"),
-  checkboxInput("com","composite display",FALSE),
-  fluidRow(
-    column(3,
-           selectizeInput(inputId = "sex",
-                          label = "waehlen Geschlecht:",
-                          choices = c("All"="",u_sex))
-    ),
+  sidebarLayout(
+    
+  
+  sidebarPanel(
+    selectizeInput(inputId = "sex",
+                   label = "waehlen Geschlecht:",
+                   choices = c("All"="",u_sex)
+                  ),
     
     ## single person auswahl
     #################################
     
     conditionalPanel(
       condition = "input.com==0",
-      column(3,
              selectizeInput(inputId = "age",
                             label = "waehlen Alt:",
-                            choices = c("All"="",u_age))
-      ),
-      column(3,
+                            choices = c("All"="",u_age)),
              selectInput(inputId = "id",
                          label = "waehlen ID",
                          choices = c("All"="",u_IDs))
-      )
     ),
     
     #############################
@@ -48,50 +45,47 @@ fluidPage(
     #############################
     conditionalPanel(
       condition = "input.com==1",
-      column(5,
              sliderInput("age_range","Age Range",
                          min = min(u_age),max=max(u_age),
-                         value = c(min(u_age),max(u_age)))
-      ),
-      column(4,
+                         value = c(min(u_age),max(u_age))),
              radioButtons("com_way","median or mean",choices = c("median","mean"),
-                          selected = "median",inline = TRUE))
-    )
+                          selected = "median",inline = TRUE)),
     
     
-  ),
-  fluidRow(
+    
+ 
     ## single person auswahl
     #################################
     conditionalPanel(
       condition = "input.com==0",
-      column(2,checkboxInput("single_region","single region",FALSE)),
+      checkboxInput("single_region","single region",FALSE),
       
-      column(4,
              conditionalPanel(
                condition = "input.single_region==1",
                selectInput(inputId = "region",
                            label = "waehlen Region",
                            choices = u_region)
-             ))
+             )
     ),
-    
+    checkboxInput("com","composite display",FALSE),
     #############################
     ##composite display
     #############################
-
+     
+    tags$button("Restart", id="restart", type="button", class="btn btn-danger action-button", onclick="history.go(0)"),
     
-  ),
-  fluidRow(
-    column(3, 
-           tags$button("Restart", id="restart", type="button", class="btn btn-danger action-button", onclick="history.go(0)")
-    )
-  ),
+    actionButton("ab","3d brain zeigen"),
   
-  DT::dataTableOutput("table"),
-  actionButton("ab","3d brain zeigen"),
-  plotlyOutput("ggseg3d"),
-  plotOutput("distributionPlot")
+
   
-  
+),
+
+mainPanel(  
+  tabsetPanel(type = "tabs",
+              tabPanel("Table",DT::dataTableOutput("table")),
+              tabPanel("3D",plotlyOutput("ggseg3d")),
+              tabPanel("DistributionPlot",plotOutput("distributionPlot")))
+  )
+
+)
 )
