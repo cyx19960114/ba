2164aa58c21b9288f5321e3b607e67e448a53852
cyx19960114 <cyx19960114@foxmail.com>
2020-04-24 00:07:39 +0200

    lasso Regression

--- server.R
+++ server.R
@@ -5,8 +5,8 @@
 library(shiny)
 library(dplyr)
 library(tidyr)
-library(ggseg3d)
-library(ggsegExtra)
+# library(ggseg3d)
+# library(ggsegExtra)
 library(plotly)
 library(ggplot2)
 library(readxl)
@@ -16,6 +16,9 @@ library(processx)
 library(reshape2)
 library(cowplot)
 library(ggpubr)
+library("XLConnect")
+library(glmnet)
+library(gplots)
 
 # source("https://gist.githubusercontent.com/benmarwick/2a1bb0133ff568cbe28d/raw/fb53bd97121f7f9ce947837ef1a4c65a73bffb3f/geom_flat_violin.R")
 source("geom_flat_violin.R")
@@ -143,6 +146,10 @@ server<-function(input, output,session) {
     input$ss_fil
   })
   
+  get_ls_fil <- reactive({
+    input$ls_fil
+  })
+  
   
   
   #######################################################
@@ -220,6 +227,12 @@ server<-function(input, output,session) {
     )
   })
   
+  output$fil_ls <- renderUI({
+    tagList(
+      selectInput("ls_fil",label = "Filter",choices = names(OASIS)[-1],multiple = TRUE)
+    )
+  })
+  
   
   
   
@@ -284,6 +297,25 @@ server<-function(input, output,session) {
   
   get_ss_choice <- reactive({    # get the select col and return the selected date
     col_input <- get_ss_fil()
+    u_oasis <- get_oasis()
+    for (col in col_input) {
+      if(as.character(col)=='sex'){
+        if((!is.null(input$sex_qc))&&input$sex_qc!="All"){
+          u_oasis <- u_oasis[u_oasis$sex==input$sex_qc,]
+        }
+      }else if(!is.null(input[[paste0(col,"_qc")]])){
+        v_min <- min(input[[paste0(col,"_qc")]])
+        v_max <- max(input[[paste0(col,"_qc")]])
+        u_oasis <- u_oasis[u_oasis[[col]]<=as.numeric(v_max),]
+        u_oasis <- u_oasis[u_oasis[[col]]>=as.numeric(v_min),]}
+      
+    }
+    
+    return(u_oasis)
+  })
+  
+  get_ls_choice <- reactive({    # get the select col and return the selected date
+    col_input <- get_ls_fil()
     u_oasis <- get_oasis()
     for (col in col_input) {
       if(as.character(col)=='sex'){
@@ -451,7 +483,38 @@ server<-function(input, output,session) {
     return(x)
   })
   
-  
+  output$ls_kon <- renderUI({
+    x <- NULL
+    for (ff in get_ls_fil()) {
+      x <- append(x,list(
+        if(as.character(ff)=="sex"){
+          selectInput("sex_qc",
+                      label = "sex",
+                      choices = unique(append("All",c("F","M"))),
+                      selected = {
+                        if (is.null(input[["sex_qc"]])||"All"==input[["sex_qc"]]){
+                          "All"
+                        } else{
+                          input[["sex_qc"]]
+                        }
+                      })
+        }else{
+          sliderInput(paste0(ff,"_qc"),paste(ff),
+                      min = min(get_oasis()[[ff]]),max=max(get_oasis()[[ff]]),
+                      value = {if(!is.null(input[[paste0(ff,"_qc")]])){
+                        input[[paste0(ff,"_qc")]]
+                      }else{
+                        c(min(get_oasis()[[ff]]),max(get_oasis()[[ff]]))
+                      }
+                      }
+          )
+        }
+      ))
+    }
+    
+    x <- append(x,list(selectInput("lasso_variable",label="Explanatory variable",choices = get_explan_names())))
+    return(x)
+  })
   
   
   ## get the composite way 
@@ -542,6 +605,11 @@ server<-function(input, output,session) {
   
   output$ss_table<- DT::renderDataTable({
     ausgewaehlte_daten <- get_ss_choice()
+    DT::datatable(ausgewaehlte_daten,class = "display nowrap",options = list(scrollX=TRUE))
+  })
+  
+  output$ls_table<- DT::renderDataTable({
+    ausgewaehlte_daten <- get_ls_choice()
     DT::datatable(ausgewaehlte_daten,class = "display nowrap",options = list(scrollX=TRUE))
   })
   
@@ -868,6 +936,9 @@ server<-function(input, output,session) {
   observeEvent(input$rp,{
     updateTabsetPanel(session,"ss_tab","Regression Plots")
   })
+  observeEvent(input$lp,{
+    updateTabsetPanel(session,"ls_tab","Lasso tabel")
+  })
   
   
   
@@ -918,6 +989,79 @@ server<-function(input, output,session) {
   })
   
   
+####lass#####  
+  
+  lasso_training_results <- function(dat,target.column,lambda_seq=10^seq(2,-2,by = -.1),alpha=1,normalise=T){
+    taco <- target.column
+    if (!is.numeric(taco)){
+      taco <- grep(target.column,colnames(dat))
+    }
+    x_vars <- as.matrix(dat[,-taco])
+    y_var <- dat[[taco]]
+    y.mean <- 0
+    y.sd <- 1
+    if (normalise){
+      x_vars <- scale(x_vars)
+      y.mean <- mean(y_var,na.rm=T)
+      y.sd <- sd(y_var,na.rm=T)
+      y_var <- (y_var - y.mean)/y.sd
+    }
+    
+    cv_output <- cv.glmnet(x_vars,y_var,alpha=alpha,lambda=lambda_seq)
+    best_lam <- cv_output$lambda.min
+    lasso_best <- glmnet(x_vars,y_var,alpha=alpha,lambda=best_lam)
+    predictions <- predict(lasso_best,s=best_lam,newx=x_vars)
+    if (normalise){
+      predictions <- y.mean + y.sd*predictions
+    }
+    return(list(lambda=best_lam,coefficients=coef(lasso_best)[,1],predictions=predictions))
+  }
+  lasso_bootstrap <- function(dat,target.column,lambda_seq=10^seq(2,-2,by = -.1),alpha=1,normalise=T,n.bootstrap=1000){
+    
+    lambda <- rep(0,n.bootstrap)
+    coefficient.matrix <- matrix(0,nrow=n.bootstrap,ncol=ncol(dat)+1)
+    colnames(coefficient.matrix) <- c("lambda","intercept",colnames(dat)[!names(dat) %in% target.column,drop=F])
+    withProgress(message = 'Calculation in progress',detail = 'Wait a moment',value = 0,expr = {
+    for (i in 1:n.bootstrap){
+      inds <- sample(nrow(dat),nrow(dat),replace=T)
+      lassi <- lasso_training_results(dat[inds,],target.column,lambda_seq=lambda_seq,alpha=alpha,normalise=normalise)
+      coefficient.matrix[i,] <- c(lassi$lambda,lassi$coefficients)
+      incProgress(1/n.bootstrap)
+    }})
+    return(coefficient.matrix)
+  }
+  prop.nonzero <- function(x){
+    return(sum(!is.na(x) & x!=0)/sum(!is.na(x)))
+  }
+  get.proportion.of.nonzero.coeffcients <- function(coefficient.matrix){
+    return(apply(coefficient.matrix,2,prop.nonzero))
+  }
+  sign.consistency <- function(x){
+    return(sum(!is.na(x) & x>=0)==sum(!is.na(x)) | sum(!is.na(x) & x<=0)==sum(!is.na(x)))
+  }
+  get.sign.consistency <- function(coefficient.matrix){
+    return(apply(coefficient.matrix,2,sign.consistency))
+  }
+  
+
+  observeEvent(input$lp, { 
+    dat<-get_ls_choice()
+    count_lasso<-which(names(dat)== input$lasso_variable)
+    if(count_lasso==8){
+      lasso.b.all <- lasso_bootstrap(dat[,-c(1:(count_lasso-1))],input$lasso_variable)
+    }else{
+      lasso.b.all <- lasso_bootstrap(dat[,-c(1:(count_lasso-1),((count_lasso+1):8))],input$lasso_variable)
+    }
+    
+    prop.nonzero.all <- get.proportion.of.nonzero.coeffcients(lasso.b.all[,-1])
+    consistent.sign.all <- get.sign.consistency(lasso.b.all[,-1])
+    bs.data <<- data.frame(prop.nonzero.all,consistent.sign.all)
+    rownames(bs.data) <- colnames(lasso.b.all)[-1]
+    label_ExplantoryVariable<-input$lasso_variable
+    output$lasso_table<- DT::renderDataTable({
+      DT::datatable(bs.data,class = "display nowrap",options = list(scrollX=TRUE),caption = paste("Explantory variable:", label_ExplantoryVariable))
+    })
+  })
   
   
 }
